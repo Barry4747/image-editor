e922a0eb6e20f4145f79276c616436b58991b7ad
Barry4747 <bartek.rasztar24@gmail.com>
2025-09-11 09:55:53 +0200

    permissions for guest in gallery

--- backend/jobs/permissions.py
+++ backend/jobs/permissions.py
@@ -9,7 +9,7 @@ class IsOwnerOrGuest(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS or request.method == 'DELETE':
             return True
         return request.user and request.user.is_authenticated
 
